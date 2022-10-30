88950e5f7f99ae090071eadcb5a5d2aa7b7558fb
Jonathan Stover <jstover@southern.edu>
2022-10-30 19:03:30 -0400

    Added switch case

--- index.js
+++ index.js
@@ -19,26 +19,35 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
     if (message.content == "Binkle"){
         message.reply("Stinkle")
-        console.log("Printed Stinkle")
     }
+
+
 
     if (message.content == "algorithm"){
         const channel = message.member.voice.channel
         if(!channel) return message.channel.send('algorithm test')
 
         const player = voiceDiscord.createAudioPlayer();
+
         const resource = voiceDiscord.createAudioResource('./sounds/algorithm.mp3')
+        switch (Math.floor(Math.random() * 2)) {
+            case 0:
+                resource = voiceDiscord.createAudioResource('./sounds/algorithm.mp3')
+                break
+            case 1:
+                resource = voiceDiscord.createAudioResource('./sounds/snoring.mp3')
+                break
+        }
 
         const connection = voiceDiscord.joinVoiceChannel({
             channelId: channel.id,
             guildId: message.guild.id,
             adapterCreator: message.guild.voiceAdapterCreator,
         })
-        console.log("Made it to the play part")
-        player.play()
+
+        player.play(resource)
         connection.subscribe(player)
 
-        console.log("Made it to the destroy part")
         player.on(voiceDiscord.AudioPlayerStatus.Idle, () => {
             connection.destroy()
         })
