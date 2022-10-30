0c1ee77c80c098904bf33c18ad139f4772b89ed7
Jonathan Stover <jstover@southern.edu>
2022-10-30 17:48:50 -0400

    Added a sound and started work on voice connection

--- index.js
+++ index.js
@@ -18,6 +18,20 @@ client.on("messageCreate", (message) => {
     if (message.content == "Binkle"){
         message.reply("Stinkle")
     }
+
+    if (message.content == "algorithm"){
+        const channel = message.member.voice.channel
+        if(!channel) return message.channel.send('algorithm test')
+
+        const player = Discord.createAudioPlayer();
+        const resource = Discord.createAudioResource('./sounds/algorithm.mp3')
+
+        const connection = Discord.joinVoiceChannel({
+            channelId: channel.id,
+            guildId: message.guild.id,
+            adapterCreator: message.guild.adapterCreator,
+        })
+    }
 })
 
 client.login(process.env.TOKEN) // Start bot with TOKEN variable stored in .env file
