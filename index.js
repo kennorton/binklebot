e32b8c9a4cff8ca9671524cdcd773531cb0a0b71
Jonathan Stover <jstover@southern.edu>
2022-10-30 16:46:28 -0400

    Removed the comment that said "butt fart haha"

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const client = new Discord.Client({
 })
 
 client.on("ready", () => {
-    console.log(`Logged in as $client.user.tag}`) // butt fart haha
+    console.log(`Logged in as $client.user.tag}`)
 })
 
 client.login(TOKEN)
